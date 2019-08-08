ca9e0fa3500572516254e9ace9dc61d8837de819
JX Wang <wjianxu@gmail.com>
2019-08-09 05:16:43 +0800

    feat: 将 build.gradle 中的 attributes 转移到 adoc 中
    - PDF 封面增加 logo
    - 优化 GitHub Pages

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
 ./gradlew generateAll
+
+echo "copy html and pdf to docs/"
+cp -f build/docs/asciidoc/example.html docs/
+cp -rf build/docs/asciidoc/images docs/
+cp -f build/docs/asciidocPdf/example.pdf docs/
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# asciidoctor gradle example
+
+- [Generate HTML 5](example.html)
+- [Generate PDF](example.pdf)
